ef6eebabdf61c1cb24dd389d73c1c877cbf6069d
mike <mikefultz33@gmail.com>
2022-08-17 18:04:53 -0700

    updated

--- deploy/01-deploy-basic-nft.js
+++ deploy/01-deploy-basic-nft.js
@@ -1,25 +1,25 @@
 const { network } = require("hardhat")
-const { verify } = require("../utils/verify")
 const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
-module.exports = async function ({ getNamedAccounts, deployments }) {
+module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
 
-    log("-----------------------")
-    const args = []
+    log("----------------------------------------------------")
+    arguments = []
     const basicNft = await deploy("BasicNft", {
         from: deployer,
-        args: args,
+        args: arguments,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
 
+    // Verify the deployment
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         log("Verifying...")
-        await verify(basicNft.address, args)
+        await verify(basicNft.address, arguments)
     }
-    log("-----------------------")
 }
 
 module.exports.tags = ["all", "basicnft", "main"]
